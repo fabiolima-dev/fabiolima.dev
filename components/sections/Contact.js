d6fb0e856a7985956dad4e18f5a6e51a73f7bb80
Fabio <fabiolima@fabiolima.dev>
2022-05-25 16:12:04 -0300

    Contact layout

--- components/sections/Contact.js
+++ components/sections/Contact.js
@@ -2,8 +2,25 @@ import SectionTitle from "../SectionTitle";
 
 export default function Contact({ section }) {
   return (
-    <div className="flex min-h-screen flex-col justify-evenly pt-16">
-      <SectionTitle section={section} />
+    <div
+      id={section.id}
+      className="flex min-h-screen flex-col justify-evenly pt-16"
+    >
+      <div className="m-auto flex max-w-md flex-col items-center gap-20">
+        <SectionTitle section={section} />
+        <p className="text-center text-gray-1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          ac magna facilisis, accumsan orci a, vehicula magna. Aliquam laoreet
+          orci sed ligula gravida fringilla. Ut non sagittis ante. Ut sed cursus
+          tortor. Quisque iaculis sed mauris nec ultrices.
+        </p>
+        <div className="flex gap-4 text-2xl">
+          <i className="ri-github-line"></i>
+          <i className="ri-linkedin-line"></i>
+          <i className="ri-twitter-line"></i>
+          <i className="ri-mail-line"></i>
+        </div>
+      </div>
     </div>
   );
 }
