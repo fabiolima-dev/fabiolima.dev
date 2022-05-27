6aeb4ab06bb3a254c5b697f97c079f281453295f
Fabio <fabiolima@fabiolima.dev>
2022-05-27 11:43:57 -0300

    Section title animation fix

--- components/SectionTitle.js
+++ components/SectionTitle.js
@@ -1,44 +1,40 @@
-import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function SectionTitle({ section }) {
-  const reveal = {
-    animate: {
-      width: ["0%", `100%`, `100%`, "0%"],
-      marginLeft: ["0px", "0px", "auto", "auto"],
-      transition: {
-        delay: 0.5,
-        duration: 1,
-        times: [0, 0.3, 0.6, 1, 1],
-      },
-    },
-  };
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
 
-  const fill = {
-    animate: {
-      background: `linear-gradient(to right, var(--${section.color}-one), var(--${section.color}-two), var(--${section.color}-three))`,
-      WebkitBackgroundClip: "text",
-      WebkitTextFillColor: "transparent",
-      textShadow: "none",
-      transition: {
-        delay: 1,
-      },
-    },
-  };
+  useEffect(() => {
+    if (inView) {
+      controls.start({
+        width: ["0%", `100%`, `100%`, "0%", "0%"],
+        marginLeft: ["0px", "0px", "auto", "auto", "0px"],
+        transition: {
+          delay: 0.5,
+          times: [0, 0.3, 0.6, 1, 1],
+        },
+      });
+    }
+  }, [controls, inView]);
 
   return (
-    <div className="w-fit">
-      <motion.h1
-        whileInView="animate"
-        variants={fill}
-        className="w-max text-center leading-normal text-primary [text-shadow:1px_1px_var(--color-secondary),1px_-1px_var(--color-secondary),-1px_1px_var(--color-secondary),-1px_-1px_var(--color-secondary)]"
+    <div ref={ref} className="w-fit">
+      <h1
+        className={`${
+          inView
+            ? `${section.color} bg-gradient-to-r bg-clip-text text-transparent delay-1000`
+            : "drop-shadow-secondary text-primary [text-shadow:-1px_-1px_0_var(--color-secondary),1px_-1px_0_var(--color-secondary),-1px_1px_0_var(--color-secondary),1px_1px_0_var(--color-secondary)]"
+        } w-max text-center leading-normal`}
       >
         {section.title}
-      </motion.h1>
+      </h1>
       <div className="relative h-0 w-full">
         <motion.div
-          whileInView="animate"
-          variants={reveal}
-          className={`${section.color} relative bottom-[72px] ml-0 h-[65px] w-0 rounded-md bg-gradient-to-r`}
+          initial={{ width: "0%", marginLeft: "0px" }}
+          animate={controls}
+          className={`${section.color} relative bottom-[72px] h-[65px] rounded-md bg-gradient-to-r`}
         ></motion.div>
       </div>
     </div>
