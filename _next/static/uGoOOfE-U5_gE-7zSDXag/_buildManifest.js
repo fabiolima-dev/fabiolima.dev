9f2b8e6729ea257630ab5bf5a9d89fd2c0811b71
Fabio <fabiolima@fabiolima.dev>
2024-01-10 05:59:49 -0300

    Updates

---
+++ _next/static/uGoOOfE-U5_gE-7zSDXag/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/599-85997ac62e7f2dd7.js","static/chunks/pages/index-36fd813bb4d8b01a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
